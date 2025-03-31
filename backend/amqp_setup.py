90ba03271dad15a70affaca63fcb63085e1ebbf6
b4ins <ranveersbains@gmail.com>
2025-03-31 16:47:01 +0800

    add check amqp setup

--- backend/amqp_setup.py
+++ backend/amqp_setup.py
@@ -92,3 +92,29 @@ create_queue(
     queue_name="O_queue",
     routing_key="*.queue",
 )
+
+def check_setup():
+    # The shared connection and channel created when the module is imported may be expired, 
+    # timed out, disconnected by the broker or a client;
+    # - re-establish the connection/channel is they have been closed
+    global connection, channel, hostname, port, exchangename, exchangetype
+
+    if not is_connection_open(connection):
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host=hostname, port=port, heartbeat=3600, blocked_connection_timeout=3600))
+    if channel.is_closed:
+        channel = connection.channel()
+        channel.exchange_declare(exchange=exchangename, exchange_type=exchangetype, durable=True) ###
+
+
+def is_connection_open(connection):
+    # For a BlockingConnection in AMQP clients,
+    # when an exception happens when an action is performed,
+    # it likely indicates a broken connection.
+    # So, the code below actively calls a method in the 'connection' to check if an exception happens
+    try:
+        connection.process_data_events()
+        return True
+    except pika.exceptions.AMQPError as e:
+        print("AMQP Error:", e)
+        print("...creating a new connection.")
+        return False
